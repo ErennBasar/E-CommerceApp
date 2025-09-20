fedb4c3833dbeda49161506e14cb2580dd9381b2
ErennBasar <2200656020@nku.edu.tr>
2025-09-20 16:43:29 +0300

    Tracking performans optimizasyonu

--- E-CommerceAPI/Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
+++ E-CommerceAPI/Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
@@ -6,14 +6,14 @@ namespace ECommerceAPI.Application.Repositories;
 // Bu işlemleri ORM'de yapıyor ama bu şekilde olunce customize edilebiliyor
 public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
 {
-    IQueryable<T> GetAll();
+    IQueryable<T> GetAll(bool tracking = true);
     
     //bir şart olsun bu şartı sağlayan birden fazla veri getirilsin
-    IQueryable<T> GetWhere(Expression<Func<T, bool>> method); // bu özel tanımlı fonksiyona verilen şart ifadesi doğru olan datalar sorgulanıp getirilecek
+    IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true); // bu özel tanımlı fonksiyona verilen şart ifadesi doğru olan datalar sorgulanıp getirilecek
 
     // FisrtOrDefault'un asenkron fonksiyonunu kullanacak
-    Task<T> GetSingleAsync(Expression<Func<T, bool>> method); //şartı sağlayan tek bir veri getirilsin
+    Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true); //şartı sağlayan tek bir veri getirilsin
     
-    Task<T> GetByIdAsync(string id); //id ile veri getirilsin 
+    Task<T> GetByIdAsync(string id, bool tracking = true); //id ile veri getirilsin 
 
 }
--- E-CommerceAPI/Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
+++ E-CommerceAPI/Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using ECommerceAPI.Application.Abstractions;
 using ECommerceAPI.Application.Repositories.Product;
+using ECommerceAPI.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceAPI.API.Controllers
@@ -34,13 +35,24 @@ namespace ECommerceAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new(){ Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10},
-                new(){ Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 20},
-                new(){ Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 30},
-            });
+            // await _productWriteRepository.AddRangeAsync(new()
+            // {
+            //     new(){ Id = Guid.NewGuid(), Name = "Product 4", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10},
+            //     new(){ Id = Guid.NewGuid(), Name = "Product 5", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 20},
+            //     new(){ Id = Guid.NewGuid(), Name = "Product 6", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 30},
+            // });
+            // await _productWriteRepository.SaveAsync();
+            
+            Product p = await _productReadRepository.GetByIdAsync("c05efc83-437f-4ce1-939e-183eaaab3e89");
+            p.Name = "Product 7";
             await _productWriteRepository.SaveAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var product = await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
         }
     }
 }
