630a1e0ac897340345b5944476508826c4428d5a
ErennBasar <2200656020@nku.edu.tr>
2025-09-21 17:55:07 +0300

    Admin ve UI Altyapısı

---
+++ src/app/admin/layout/layout-module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ComponentsModule} from './components/components-module';
+import {Layout} from './layout';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ComponentsModule,
+  ],
+  exports: []
+})
+export class LayoutModule { }
