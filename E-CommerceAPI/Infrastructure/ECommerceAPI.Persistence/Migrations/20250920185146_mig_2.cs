53084684923f4dcd15763e9e0985ee8d270b0562
ErennBasar <2200656020@nku.edu.tr>
2025-09-21 12:50:25 +0300

    migrations

---
+++ E-CommerceAPI/Infrastructure/ECommerceAPI.Persistence/Migrations/20250920185146_mig_2.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceAPI.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Products",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Products",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Orders",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CustomerId",
+                table: "Orders",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Customers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Price",
+                table: "Products",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Description",
+                table: "Orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CustomerId",
+                table: "Orders",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+        }
+    }
+}
